9e93c95a1efd77bfc69a80ec49b3826ee71249c7
ahmadfadhilah <stevenasep000@gmail.com>
2022-07-20 22:35:13 +0700

    init gesture handler screen

--- App.tsx
+++ App.tsx
@@ -1,117 +1,178 @@
-import React, { useState } from 'react';
-import { Dimensions, StyleSheet, Switch, } from 'react-native';
-import Animated, {
-  interpolateColor,
-  useAnimatedStyle,
-  useDerivedValue,
-  withTiming,
-} from 'react-native-reanimated';
+// import React, { useState } from 'react';
+// import { Dimensions, StyleSheet, Switch, } from 'react-native';
+// import Animated, {
+//   interpolateColor,
+//   useAnimatedStyle,
+//   useDerivedValue,
+//   withTiming,
+// } from 'react-native-reanimated';
 
-const Colors = {
-  dark: {
-    background: '#1E1E1E',
-    circle: '#252525',
-    text: '#F8F8F8',
-  },
-  light: {
-    background: '#F8F8F8',
-    circle: '#FFF',
-    text: '#1E1E1E',
-  },
-};
+// const Colors = {
+//   dark: {
+//     background: '#1E1E1E',
+//     circle: '#252525',
+//     text: '#F8F8F8',
+//   },
+//   light: {
+//     background: '#F8F8F8',
+//     circle: '#FFF',
+//     text: '#1E1E1E',
+//   },
+// };
 
-const SWITCH_TRACK_COLOR = {
-  true: 'rgba(256, 0, 256, 0.2)',
-  false: 'rgba(0,0,0,0.1)',
-};
+// const SWITCH_TRACK_COLOR = {
+//   true: 'rgba(256, 0, 256, 0.2)',
+//   false: 'rgba(0,0,0,0.1)',
+// };
 
-type Theme = 'light' | 'dark';
+// type Theme = 'light' | 'dark';
 
-export default function App() {
-  const [theme, setTheme] = useState<Theme>('light');
+// export default function App() {
+//   const [theme, setTheme] = useState<Theme>('light');
 
-  // const progress = useSharedValue(0);
-  const progress = useDerivedValue(() => {
-    return theme === 'dark' ? withTiming(1) : withTiming(0);
-  }, [theme]);
+//   // const progress = useSharedValue(0);
+//   const progress = useDerivedValue(() => {
+//     return theme === 'dark' ? withTiming(1) : withTiming(0);
+//   }, [theme]);
 
-  const rStyle = useAnimatedStyle(() => {
-    const backgroundColor = interpolateColor(
-      progress.value,
-      [0, 1],
-      [Colors.light.background, Colors.dark.background]
-    );
+//   const rStyle = useAnimatedStyle(() => {
+//     const backgroundColor = interpolateColor(
+//       progress.value,
+//       [0, 1],
+//       [Colors.light.background, Colors.dark.background]
+//     );
 
-    return { backgroundColor };
+//     return { backgroundColor };
+//   });
+
+//   const rCircleStyle = useAnimatedStyle(() => {
+//     const backgroundColor = interpolateColor(
+//       progress.value,
+//       [0, 1],
+//       [Colors.light.circle, Colors.dark.circle]
+//     );
+
+//     return { backgroundColor };
+//   });
+
+//   const rTextStyle = useAnimatedStyle(() => {
+//     const color = interpolateColor(
+//       progress.value,
+//       [0, 1],
+//       [Colors.light.text, Colors.dark.text]
+//     );
+
+//     return { color };
+//   });
+
+//   return (
+//     <Animated.View style={[styles.container, rStyle]}>
+//       <Animated.Text style={[styles.text, rTextStyle]}>Happy</Animated.Text>
+//       <Animated.View style={[styles.circle, rCircleStyle]}>
+//         <Switch
+//           value={theme === 'dark'}
+//           onValueChange={(toggled) => {
+//             setTheme(toggled ? 'dark' : 'light');
+//           }}
+//           trackColor={SWITCH_TRACK_COLOR}
+//           thumbColor={'violet'}
+//         />
+//       </Animated.View>
+//     </Animated.View>
+//   );
+// }
+
+// const SIZE = Dimensions.get('window').width * 0.7;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   circle: {
+//     width: SIZE,
+//     height: SIZE,
+//     backgroundColor: '#FFF',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     borderRadius: SIZE / 2,
+//     shadowOffset: {
+//       width: 0,
+//       height: 20,
+//     },
+//     shadowRadius: 10,
+//     shadowOpacity: 0.1,
+//     elevation: 8,
+//   },
+//   text: {
+//     fontSize: 60,
+//     textTransform: 'uppercase',
+//     fontWeight: '700',
+//     letterSpacing: 14,
+//     marginBottom: 35,
+//   },
+// });
+
+import React from "react";
+import { View, Text, Animated, PanResponder } from "react-native";
+
+export default function BelumBayar() {
+  const position = new Animated.ValueXY({ x: 0, y: 0 });
+
+  //2. Handling gestures
+  const pan = PanResponder.create({
+    onMoveShouldSetPanResponder: () => true,
+
+    onPanResponderMove: (e, gesture) => {
+      position.setValue({ x: gesture.dx, y: gesture.dy });
+    },
+    // onPanResponderMove: Animated.event([
+    //   null,
+    //   {dx: position.x, dy: position.y},
+    // ]),
+
+    onPanResponderRelease: () => {
+      // position.setValue({x:0,y:0})
+      Animated.spring(position, {
+        toValue: { x: 0, y: 0 },
+        useNativeDriver: true,
+      }).start();
+    },
   });
 
-  const rCircleStyle = useAnimatedStyle(() => {
-    const backgroundColor = interpolateColor(
-      progress.value,
-      [0, 1],
-      [Colors.light.circle, Colors.dark.circle]
-    );
-
-    return { backgroundColor };
+  //1. interpolate over value
+  const rotate = position.x.interpolate({
+    inputRange: [0, 90],
+    outputRange: ["0deg", "360deg"],
   });
-
-  const rTextStyle = useAnimatedStyle(() => {
-    const color = interpolateColor(
-      progress.value,
-      [0, 1],
-      [Colors.light.text, Colors.dark.text]
-    );
-
-    return { color };
-  });
-
   return (
-    <Animated.View style={[styles.container, rStyle]}>
-      <Animated.Text style={[styles.text, rTextStyle]}>Happy</Animated.Text>
-      <Animated.View style={[styles.circle, rCircleStyle]}>
-        <Switch
-          value={theme === 'dark'}
-          onValueChange={(toggled) => {
-            setTheme(toggled ? 'dark' : 'light');
-          }}
-          trackColor={SWITCH_TRACK_COLOR}
-          thumbColor={'violet'}
-        />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Animated.View
+        {...pan.panHandlers}
+        style={{
+          height: 80,
+          width: 80,
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#61dafb",
+          borderRadius: 4,
+          transform: [
+            { translateX: position.x },
+            { translateY: position.y },
+            { rotate: rotate },
+          ],
+        }}
+      >
+        <Text>Halo</Text>
       </Animated.View>
-    </Animated.View>
+    </View>
   );
 }
-
-const SIZE = Dimensions.get('window').width * 0.7;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  circle: {
-    width: SIZE,
-    height: SIZE,
-    backgroundColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: SIZE / 2,
-    shadowOffset: {
-      width: 0,
-      height: 20,
-    },
-    shadowRadius: 10,
-    shadowOpacity: 0.1,
-    elevation: 8,
-  },
-  text: {
-    fontSize: 60,
-    textTransform: 'uppercase',
-    fontWeight: '700',
-    letterSpacing: 14,
-    marginBottom: 35,
-  },
-});
-
